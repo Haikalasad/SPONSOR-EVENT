8352d4385316de37ea7218fced4a117c39082663
Haikalasad <muhammad.haikal.21@student.if.ittelkom-sby.ac.id>
2024-06-03 13:43:12 +0700

    add storage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap">
-  <link rel="stylesheet" href="CSS/home_style.css" />
+  <link rel="stylesheet" href="/CSS/home_style.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/fadd57a8f9.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
@@ -31,7 +31,7 @@
   <!-- navbar section -->
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <img src="ASSETS/logo.png" style=" width: 180px;height: 40px;" />
+      <img src="/ASSETS/logo.png" style=" width: 180px;height: 40px;" />
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>

04d2d6a0e8e71e495ab68fd62d561ad9e931204c
Haikalasad <muhammad.haikal.21@student.if.ittelkom-sby.ac.id>
2024-06-12 22:41:29 +0700

    fixing minor bug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -13,7 +13,7 @@ class EventController extends Controller
     {
         $newEvents = Event::orderBy('created_at', 'desc')->take(4)->get();
         
-        $events = Event::all();
+        $events = Event::orderBy('created_at', 'desc')->get();
     
         return view('event', ['newEvents' => $newEvents, 'events' => $events]);
     }
